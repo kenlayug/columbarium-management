2f34a2a97a23c88b5587ef691b96cdd21186dad7
Ken Layug <ken_layug@yahoo.com>
2016-02-22 15:51:12 +0800

    item create.

--- columbarium-web/src/columbarium/actions/item/CreateItemAction.java
+++ columbarium-web/src/columbarium/actions/item/CreateItemAction.java
@@ -1,11 +1,16 @@
 package columbarium.actions.item;
 
 import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
 
 import columbarium.model.Item;
 
-public class CreateItemAction implements Action{
+public class CreateItemAction extends ActionSupport implements Action{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Item item;
 	
 	public Item getItem() {
@@ -20,6 +25,20 @@ public class CreateItemAction implements Action{
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
 		return getItem().create();
+	}
+	
+	public void validate(){
+		
+		if (item.getStrItemName().length() == 0 || item.getStrItemName() == null){
+
+		}
+		if (item.getStrItemDesc().length() == 0 || item.getStrItemDesc() == null){
+
+		}
+		if (Double.parseDouble(item.getDblPrice()) < 0 || item.getDblPrice() == null){
+
+		}
+		
 	}
 
 }
